1c71aca47085709c486e67cabe815648003f413c
XxFiEnDxX <amitkumargdgps@gmail.com>
2025-10-03 00:59:00 +0530

    HTTP response

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,19 +5,33 @@ import (
 	"io"
 	"net"
 
+	request "tcp.to.http/internal/requests"
 	"tcp.to.http/internal/response"
 )
 
-type Server struct {
-	closed bool
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
 }
 
-func runConnection(_s *Server, conn io.ReadWriteCloser) {
-	defer conn.Close()
+type Handler func(w *response.Writer, req *request.Request)
 
-	headers := response.GetDefaultHeaders(0)
-	response.WriteStatusLine(conn, response.StatusOK)
-	response.WriteHeaders(conn, headers)
+type Server struct {
+	closed  bool
+	handler Handler
+}
+
+func runConnection(s *Server, conn io.ReadWriteCloser) {
+	defer conn.Close()
+	responseWriter := response.NewWriter(conn)
+	r, err := request.RequestFromReader(conn)
+	if err != nil {
+		responseWriter.WriteStatusLine(response.StatusBadRequest)
+		responseWriter.WriteHeaders(*response.GetDefaultHeaders(0))
+		return
+	}
+
+	s.handler(responseWriter, r)
 }
 
 func runServer(s *Server, listener net.Listener) {
@@ -33,12 +47,15 @@ func runServer(s *Server, listener net.Listener) {
 	}
 }
 
-func Serve(port uint16) (*Server, error) {
+func Serve(port uint16, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
 	}
-	server := &Server{closed: false}
+	server := &Server{
+		closed:  false,
+		handler: handler,
+	}
 	go runServer(server, listener)
 
 	return server, nil
